b129948cb2f6cf39b8bf41a196404bf7fc11887f
Gony-Rida-ANB <ridagony@gmail.com>
2025-06-10 16:50:50 +0700

    feature issue #3: Event Listener for Buttons and Element References

---
+++ script.js
@@ -0,0 +1,4 @@
+const expressionEl = document.querySelector(".expression");
+const resultEl = document.querySelector(".result");
+const buttons = document.querySelectorAll("button");
+let expression = "";
